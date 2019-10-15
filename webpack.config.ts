69454bd6cea34e0454a239728adc33bee76565b5
zhangyang <zhangyang@cloudwalk.cn>
2019-10-15 14:06:07 +0800

    :tada:初始化项目结构

---
+++ webpack.config.ts
@@ -0,0 +1,46 @@
+var path = require('path')
+
+import htmlWebpackPlugin from 'html-webpack-plugin'
+import cleanWebpackPlugin from 'clean-webpack-plugin'
+
+export default {
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'lib'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /.html?$/,
+        use: 'html-loader',
+      },
+      {
+        test: /.ejs?$/,
+        use: 'ejs-loader',
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './lib',
+    hot: true,
+    port: 3000,
+    open: true,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'ts插件库测试',
+      template: './example/index.ejs',
+    }),
+    new cleanWebpackPlugin(),
+  ],
+  devtool: 'inline-source-map',
+}
